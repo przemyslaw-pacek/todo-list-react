5ad0b0025d26f964c5336958801fe3b81f491d3b
Przemysław Pacek <ppacek@op.pl>
2023-09-11 20:27:11 +0200

    ' changed for "

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Form from './Form';
-import List from './List';
-import Buttons from './Buttons';
-import Section from './Section';
-import Header from './Header';
-import Container from './Container';
+import Form from "./Form";
+import List from "./List";
+import Buttons from "./Buttons";
+import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
