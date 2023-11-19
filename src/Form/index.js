88ead930998f502db2ff7f368a969cd001873de0
Przemysław Pacek <ppacek@op.pl>
2023-11-19 14:41:59 +0100

    modified condition

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,7 +6,7 @@ const Form = ({ addNewTask }) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    if (newTaskContent.trim() !== "") {
+    if (newTaskContent.trim()) {
       addNewTask(newTaskContent.trim())
     };
     setNewTaskContent("");
