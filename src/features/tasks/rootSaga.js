3ce4e42a949ec1068b37a5e9d359bbeafcce2076
Przemysław Pacek <ppacek@op.pl>
2024-01-11 20:44:07 +0100

    renamed to tasksSaga

--- src/features/tasks/rootSaga.js
+++ src/features/tasks/rootSaga.js
@@ -1,9 +1,9 @@
 import { all } from "redux-saga/effects";
-import { watchFetchExampleTasks } from "./tasksSaga";
+import { tasksSaga } from "./tasksSaga";
 
 
 export default function* rootSaga() {
     yield all([
-        watchFetchExampleTasks(),
+        tasksSaga(),
     ]);
 }
