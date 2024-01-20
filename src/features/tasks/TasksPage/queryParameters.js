5202959e89cf18b77eab3614114251be9404c553
Przemysław Pacek <ppacek@op.pl>
2024-01-20 19:57:23 +0100

    fixed function queryParamemters

--- src/features/tasks/TasksPage/queryParameters.js
+++ src/features/tasks/TasksPage/queryParameters.js
@@ -1,19 +1,18 @@
 import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
-export const useQueryParameter = () => {
+export const useQueryParameter = (key) => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
 
-    searchParams.get(toString);
+    return searchParams.get(key)
 };
 
 export const useReplaceQueryParameter = () => {
-    const location = useLocation();
     const history = useHistory();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
 
     return ({ key, value }) => {
-        const searchParams = new URLSearchParams(location.search);
-
         if (value === undefined) {
             searchParams.delete(key);
         } else {
