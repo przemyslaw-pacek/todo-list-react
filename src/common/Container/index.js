7b5cc810ee4de91408336ff54d872b5211b828f3
Przemysław Pacek <ppacek@op.pl>
2024-01-23 20:17:12 +0100

    chenged content

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
-const Container = styled.main` 
+export default styled.main` 
     max-width: 900px;
     padding: 20px;
     margin: 0 auto;
 `;
-
-export default Container;
