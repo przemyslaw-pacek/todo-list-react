dc52d98c5c10c03ff8da25ccc334d8333c6e9f51
Przemysław Pacek <ppacek@op.pl>
2024-10-12 23:41:42 +0200

    added and changed Task[]

--- src/features/tasks/tasksLocalStorage.ts
+++ src/features/tasks/tasksLocalStorage.ts
@@ -5,7 +5,7 @@ const localStorageKey = "tasks";
 export const saveTasksInLocalStorage = (tasks: Task[]) =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-export const getTasksFromLocalStorage = (): Task[] => {
+export const getTasksFromLocalStorage = () => {
   const storedTasks = localStorage.getItem(localStorageKey);
-  return storedTasks ? JSON.parse(storedTasks) : [];
+  return (storedTasks ? JSON.parse(storedTasks) : []) as Task[];
 };
