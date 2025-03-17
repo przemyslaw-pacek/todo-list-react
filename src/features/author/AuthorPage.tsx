f8b1aff58a28f4f7b35f8f4c77f993fbcac84196
Przemysław Pacek <ppacek@op.pl>
2025-03-17 20:58:55 +0100

    updated author information

--- src/features/author/AuthorPage.tsx
+++ src/features/author/AuthorPage.tsx
@@ -9,10 +9,10 @@ export default () => (
       title="Przemysław Pacek"
       body={
         <>
-          Od roku pracuję jako operator maszyn <i>CNC</i>. Wcześniej pracowałem
-          w wielu firmach i na wielu stanowiskach, między innymi w branży{" "}
-          <i>finansowej</i> oraz <i>RTV-AGD</i>. Jednak zawsze z tyłu głowy
-          miałem myśl o programowaniu. Impulsem do podjęcia kursu na{" "}
+          W życiu zajmowałem się różnymi rzeczami, pracowałem w wielu firmach i
+          na różnych stanowiskach, między innymi w branży <i>finansowej</i> oraz{" "}
+          <i>RTV-AGD</i>. Jednak zawsze z tyłu głowy miałem myśl o
+          programowaniu. Impulsem do podjęcia kursu na{" "}
           <strong>Frontend Developer</strong> było szkolenie na operatora maszyn{" "}
           <i>CNC</i>, gdzie zetknąłem się z programowaniem i to mi się bardzo
           spodobało 🙂
